30c25d2701899faae644be46ec7fa8af356677f4
runru1030 <runru1030@gmail.com>
2023-12-09 02:17:39 +0900

    chore : 스타일 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
 import { Providers } from "@components/ApolloProvider";
 import "@styles/globals.css";
+import localFont from "next/font/local";
+
+const pretendard = localFont({
+  src: "../../public/font/PretendardVariable.woff2",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={pretendard.className}>
       <head />
       <body>
         <Providers>{children}</Providers>
