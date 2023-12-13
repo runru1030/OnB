5923b66ab7b8d8c02b2550d773f8fddb63977460
runru1030 <runru1030@gmail.com>
2023-12-13 23:57:54 +0900

    chore : apollo provider&rsc client setting

--- src/app/_components/ApolloProvider.tsx
+++ src/app/_components/ApolloProvider.tsx
@@ -1,11 +1,11 @@
 "use client";
-import React from "react";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-export const Providers = ({ children }: { children: any }) => {
+export const ApolloProviders = ({ children }: { children: any }) => {
   const client = new ApolloClient({
     uri: "http://localhost:3000/api/graphql",
     cache: new InMemoryCache(),
+    ssrMode: true,
   });
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
 };
