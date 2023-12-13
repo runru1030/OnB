c0a5b67a06a68fd79e83fecae81d8b9e6a277c49
runru1030 <runru1030@gmail.com>
2023-12-14 01:07:15 +0900

    chore : dateSelector 수정

--- src/components/useDateSelect.ts
+++ src/components/useDateSelect.ts
@@ -1,19 +1,29 @@
-import React, { useState } from "react";
-
+import { useState } from "react";
+import { Range } from "react-date-range";
+interface SelectionRange {
+  [key: string]: Range;
+}
 const useDateSelect = () => {
   const [selectionRange, setSelectionRange] = useState({
     startDate: new Date(),
     endDate: new Date(),
     key: "selection",
   });
-  const handleSelectDate = (ranges: any) => {
+  const handleSelectDate = (ranges: SelectionRange) => {
     setSelectionRange({
-      startDate: ranges["selection"].startDate,
-      endDate: ranges["selection"].endDate,
-      key: ranges["selection"].key,
+      startDate: ranges?.["selection"].startDate ?? new Date(),
+      endDate: ranges?.["selection"].endDate ?? new Date(),
+      key: ranges?.["selection"].key ?? "selection",
     });
   };
-  return { selectionRange, handleSelectDate };
+  const resetSelectDate = () => {
+    setSelectionRange({
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    });
+  };
+  return { selectionRange, handleSelectDate, resetSelectDate };
 };
 
 export default useDateSelect;
