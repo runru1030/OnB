4ff39e9b7bc03258e36a8b39ac45eec58e18bb2c
runru1030 <runru1030@gmail.com>
2023-12-14 14:08:41 +0900

    design : 홈화면 ui 수정

--- src/app/(routing)/home/_components/TripBox.tsx
+++ src/app/(routing)/home/_components/TripBox.tsx
@@ -4,16 +4,15 @@ import { TripQueryData } from "../_types";
 const dateformatter = (date: Date) =>
   `${date.getFullYear()}.${date.getMonth()}.${date.getDate()}`;
 
-// const Flag = (countryId: string) => <Image src={}/>;
 const TripBox = ({ title, startedAt, endedAt, Country }: TripQueryData) => {
   return (
-    <div className="bg-grey-light-100 border border-grey-light-600 flex flex-col h-[200px] p-4 justify-between items-center rounded-2xl shadow-normal">
-      <div className=" text-lg font-medium">{title}</div>
+    <div className="bg-grey-light-300 border border-grey-light-400 flex flex-col h-[160px] p-4 justify-between items-center rounded-2xl">
+      <div className="font-medium">{title}</div>
       <div className="flex flex-col gap-2 items-center">
-        <Image src={Country.flag_img ?? ""} width={60} height={10} alt="국기" />
-        <span className="text-sm text-grey-500">{Country.name}</span>
+        <Image src={Country.flag_img ?? ""} width={50} height={10} alt="국기" />
+        <span className="text-xs text-grey-500">{Country.name}</span>
       </div>
-      <div className="flex gap-1 text-grey-400 tracking-wide font-light">
+      <div className="flex gap-1 text-grey-400 tracking-wide font-light text-sm">
         <span>{dateformatter(new Date(startedAt))}</span>-
         <span>{dateformatter(new Date(endedAt))}</span>
       </div>
