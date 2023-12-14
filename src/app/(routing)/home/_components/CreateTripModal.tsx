63eeef6c3c18945ff2d29ffe86bf567289f7761a
runru1030 <runru1030@gmail.com>
2023-12-14 20:15:59 +0900

    refactor : StepModal 컴포넌트

--- src/app/(routing)/home/_components/CreateTripModal.tsx
+++ src/app/(routing)/home/_components/CreateTripModal.tsx
@@ -4,20 +4,18 @@ import { useQuery } from "@apollo/experimental-nextjs-app-support/ssr";
 import Button from "@components/Button";
 import DateSelector from "@components/DateSelector";
 import { Input } from "@components/Input";
-import Modal from "@components/Modal";
 import useDateSelect from "@components/useDateSelect";
 import { CREATE_TRIP } from "@lib/graphql/mutations";
 import { GET_COUNTRIES } from "@lib/graphql/queries";
 import AddSharpIcon from "@mui/icons-material/AddSharp";
-import CloseSharpIcon from "@mui/icons-material/CloseSharp";
 import { Country } from "@prisma/client";
 import clsx from "clsx";
 import { useAtom, useAtomValue, useSetAtom } from "jotai";
 import { atomWithReset, useResetAtom } from "jotai/utils";
 import Image from "next/image";
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useEffect } from "react";
 import { myTripStore, myTripsAtom } from "./MyTripProvider";
-import ArrowBackIosNewSharpIcon from "@mui/icons-material/ArrowBackIosNewSharp";
+import StepModal from "@components/Modal/StepModal";
 
 const tripReqAtom = atomWithReset({
   title: "",
@@ -25,12 +23,8 @@ const tripReqAtom = atomWithReset({
   startedAt: new Date(),
   endedAt: new Date(),
 });
-const stepAtom = atomWithReset(1);
 
-const CreateTripModal = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [step, setStep] = useAtom(stepAtom);
-
+const CreateTripStepModal = () => {
   const tripData = useAtomValue(tripReqAtom);
   const resetTripData = useResetAtom(tripReqAtom);
   const userId = "1";
@@ -39,13 +33,11 @@ const CreateTripModal = () => {
   const [createTrip] = useMutation(CREATE_TRIP, {
     onCompleted: (res) => {
       setMyTrips((p) => [res.createTrip, ...p]);
-      setIsOpen(false);
     },
   });
 
-  const onCreateTrip = (e: FormEvent) => {
+  const onCreateTrip = () => {
     try {
-      e.preventDefault();
       if (tripData.title.trim() === "")
         throw new Error("여행 이름을 입력해주세요!");
       if (tripData.countryId === "")
@@ -65,154 +57,128 @@ const CreateTripModal = () => {
     }
   };
   return (
-    <Modal
-      open={isOpen}
-      onOpenChange={(open) => {
-        setIsOpen(open);
-        if (!open) {
-          resetTripData();
-          setStep(1);
-        }
-      }}
-    >
-      <Modal.Trigger>
+    <StepModal>
+      <StepModal.Trigger>
         <Button className="btn-blue flex items-center justify-center gap-1">
           <AddSharpIcon sx={{ fontSize: 18 }} />
           여행 만들기
         </Button>
-      </Modal.Trigger>
-      <Modal.Content className="w-full top-[52px] bottom-0 translate-y-0 !p-0 rounded-b-none">
-        <Modal.Title className="p-4">
-          {step > 1 && (
-            <Button
-              className="absolute top-1/2 left-4 !p-0 -translate-y-1/2"
-              onClick={() => setStep((p) => p - 1)}
-            >
-              <ArrowBackIosNewSharpIcon sx={{ fontSize: 18 }} />
-            </Button>
-          )}
-          여행 만들기
-          <Modal.Close>
-            <Button className="absolute top-1/2 right-4 !p-0 -translate-y-1/2">
-              <CloseSharpIcon />
-            </Button>
-          </Modal.Close>
-        </Modal.Title>
+      </StepModal.Trigger>
+      <StepModal.Content onCloseAutoFocus={resetTripData}>
+        <StepModal.Title className="p-4">여행 만들기</StepModal.Title>
 
-        <div className="flex flex-col gap-4 h-[calc(100%-60px)]">
-          {
+        <StepModal.StepSection
+          stepContentList={[
             {
-              1: <TitleInputContent />,
-              2: <SelectCountryContent />,
-              3: <SelectDateContent onCreateTrip={onCreateTrip} />,
-            }[step]
-          }
-        </div>
-      </Modal.Content>
-    </Modal>
+              content: <TitleInputContent />,
+              nextButton: (
+                <StepModal.StepNext
+                  requiredCondition={{
+                    condition: tripData.title.trim().length === 0,
+                    description: "이름을 입력해주세요!",
+                  }}
+                >
+                  다음
+                </StepModal.StepNext>
+              ),
+            },
+            {
+              content: <SelectCountryContent />,
+              nextButton: (
+                <StepModal.StepNext
+                  requiredCondition={{
+                    condition: tripData.countryId === "",
+                    description: "국가를 선택해주세요!",
+                  }}
+                >
+                  다음
+                </StepModal.StepNext>
+              ),
+            },
+            {
+              content: <SelectDateContent />,
+              nextButton: (
+                <StepModal.StepNext onLastStepHandler={onCreateTrip}>
+                  여행 만들기
+                </StepModal.StepNext>
+              ),
+            },
+          ]}
+        />
+      </StepModal.Content>
+    </StepModal>
   );
 };
 
-export default CreateTripModal;
+export default CreateTripStepModal;
 
 const TitleInputContent = () => {
   const [tripData, setTripData] = useAtom(tripReqAtom);
-  const setStep = useSetAtom(stepAtom);
   return (
-    <>
-      <div className="flex-1 overflow-auto p-4">
-        <div className="flex flex-col gap-4 ">
-          <h2 className="text-xl font-medium">
-            여행 이름을 입력해주세요{" "}
-            <span className="text-xs font-normal text-grey-400">최대 10자</span>
-          </h2>
-          <Input
-            placeholder="여행 이름"
-            type="text"
-            value={tripData.title}
-            onChange={(e) =>
-              setTripData((p) => ({ ...p, title: e.target.value }))
-            }
-            required
-            autoFocus
-            maxLength={10}
-            className="w-full text-lg border-blue-300 rounded-xl"
-          />
-        </div>
-      </div>
-      <Button
-        onClick={() => {
-          if (tripData.title.trim().length == 0) {
-            return alert("이름을 입력해주세요!");
+    <div className="flex-1 overflow-auto p-4">
+      <div className="flex flex-col gap-4 ">
+        <h2 className="text-xl font-medium">
+          여행 이름을 입력해주세요{" "}
+          <span className="text-xs font-normal text-grey-400">최대 10자</span>
+        </h2>
+        <Input
+          placeholder="여행 이름"
+          type="text"
+          value={tripData.title}
+          onChange={(e) =>
+            setTripData((p) => ({ ...p, title: e.target.value }))
           }
-          setStep((p) => p + 1);
-        }}
-        className="btn-blue m-4"
-      >
-        다음
-      </Button>
-    </>
+          required
+          autoFocus
+          maxLength={10}
+          className="w-full text-lg border-blue-300 rounded-xl"
+        />
+      </div>
+    </div>
   );
 };
 const SelectCountryContent = () => {
   const { data: countriesQuery } = useQuery(GET_COUNTRIES);
   const [tripData, setTripData] = useAtom(tripReqAtom);
-  const setStep = useSetAtom(stepAtom);
   return (
-    <>
-      <div className="flex-1 overflow-auto p-4">
-        <div className="flex flex-col gap-4">
-          <h2 className="text-xl font-medium">
-            여행 국가를 선택해주세요{" "}
-            <span className="text-xs font-normal text-grey-400">단일 선택</span>
-          </h2>
-          <div className="flex flex-col gap-2 ">
-            {countriesQuery?.countries?.map((country: Country) => (
-              <div
-                key={country.id}
-                onClick={() => {
-                  if (tripData.countryId === country.id) {
-                    setTripData((p) => ({ ...p, countryId: "" }));
-                  } else {
-                    setTripData((p) => ({ ...p, countryId: country.id }));
-                  }
-                }}
-                className={clsx(
-                  tripData.countryId === country.id &&
-                    "bg-grey-light-400 duration-300 text-blue",
-                  "p-1.5 rounded-lg flex justify-between"
-                )}
-              >
-                {country.name}
-                <Image
-                  src={country.flag_img ?? ""}
-                  width={40}
-                  height={100}
-                  alt="국기"
-                />
-              </div>
-            ))}
-          </div>
+    <div className="flex-1 overflow-auto p-4">
+      <div className="flex flex-col gap-4">
+        <h2 className="text-xl font-medium">
+          여행 국가를 선택해주세요{" "}
+          <span className="text-xs font-normal text-grey-400">단일 선택</span>
+        </h2>
+        <div className="flex flex-col gap-2 ">
+          {countriesQuery?.countries?.map((country: Country) => (
+            <div
+              key={country.id}
+              onClick={() => {
+                if (tripData.countryId === country.id) {
+                  setTripData((p) => ({ ...p, countryId: "" }));
+                } else {
+                  setTripData((p) => ({ ...p, countryId: country.id }));
+                }
+              }}
+              className={clsx(
+                tripData.countryId === country.id &&
+                  "bg-grey-light-400 duration-300 text-blue",
+                "p-1.5 rounded-lg flex justify-between"
+              )}
+            >
+              {country.name}
+              <Image
+                src={country.flag_img ?? ""}
+                width={40}
+                height={100}
+                alt="국기"
+              />
+            </div>
+          ))}
         </div>
       </div>
-      <Button
-        onClick={() => {
-          if (tripData.countryId === "") {
-            return alert("국가를 선택해주세요!");
-          }
-          setStep((p) => p + 1);
-        }}
-        className="btn-blue m-4"
-      >
-        다음
-      </Button>
-    </>
+    </div>
   );
 };
-interface SelectDateContentProps {
-  onCreateTrip: (e: FormEvent) => void;
-}
-const SelectDateContent = ({ onCreateTrip }: SelectDateContentProps) => {
+const SelectDateContent = () => {
   const [tripData, setTripData] = useAtom(tripReqAtom);
   const { selectionRange, handleSelectDate } = useDateSelect({
     defaultStartDate: tripData.startedAt,
@@ -228,16 +194,11 @@ const SelectDateContent = ({ onCreateTrip }: SelectDateContentProps) => {
   }, [selectionRange]);
 
   return (
-    <>
-      <div className="flex-1 overflow-auto p-4">
-        <div className="flex flex-col gap-4">
-          <h2 className="text-xl font-medium">여행 날짜를 선택해주세요 </h2>
-          <DateSelector ranges={[selectionRange]} onChange={handleSelectDate} />
-        </div>
+    <div className="flex-1 overflow-auto p-4">
+      <div className="flex flex-col gap-4">
+        <h2 className="text-xl font-medium">여행 날짜를 선택해주세요 </h2>
+        <DateSelector ranges={[selectionRange]} onChange={handleSelectDate} />
       </div>
-      <Button onClick={onCreateTrip} className="btn-blue m-4">
-        여행 만들기
-      </Button>
-    </>
+    </div>
   );
 };
--- src/app/(routing)/trip/[tid]/_components/CreateBudgetModal.tsx
+++ src/app/(routing)/trip/[tid]/_components/CreateBudgetModal.tsx
@@ -1,19 +1,17 @@
 "use client";
 import { useMutation } from "@apollo/client";
 import { useQuery } from "@apollo/experimental-nextjs-app-support/ssr";
+import StepModal from "@components/Modal/StepModal";
 import Button from "@components/Button";
 import { Input } from "@components/Input";
-import Modal from "@components/Modal";
 import { CREATE_BUDGET } from "@lib/graphql/mutations";
 import { GET_CURRENCIES } from "@lib/graphql/queries";
 import AddSharpIcon from "@mui/icons-material/AddSharp";
-import ArrowBackIosNewSharpIcon from "@mui/icons-material/ArrowBackIosNewSharp";
-import CloseSharpIcon from "@mui/icons-material/CloseSharp";
 import { Currency } from "@prisma/client";
 import clsx from "clsx";
 import { useAtom, useAtomValue, useSetAtom } from "jotai";
 import { atomWithReset, useResetAtom } from "jotai/utils";
-import { ChangeEvent, FormEvent, useMemo, useState } from "react";
+import { ChangeEvent, FormEvent, useMemo } from "react";
 import { tripAtom, tripStore } from "../TripProvider";
 
 interface budgetReqAtom {
@@ -26,12 +24,8 @@ const budgetReqAtom = atomWithReset<budgetReqAtom>({
   currencyId: "",
   type: "CASH",
 });
-const stepAtom = atomWithReset(1);
 
 const CreateBudgetModal = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [step, setStep] = useAtom(stepAtom);
-
   const setTrip = useSetAtom(tripAtom, { store: tripStore });
   const budgetData = useAtomValue(budgetReqAtom);
   const resetTripData = useResetAtom(budgetReqAtom);
@@ -39,14 +33,12 @@ const CreateBudgetModal = () => {
 
   const [createBudget] = useMutation(CREATE_BUDGET, {
     onCompleted: (res) => {
-      setIsOpen(false);
       setTrip((p) => ({ ...p, budgets: [res.createBudget, ...p.budgets] }));
     },
   });
 
-  const onCreateBudget = (e: FormEvent) => {
+  const onCreateBudget = () => {
     try {
-      e.preventDefault();
       if (budgetData.title.trim() === "")
         throw new Error("여행 이름을 입력해주세요!");
       if (budgetData.currencyId === "")
@@ -66,50 +58,42 @@ const CreateBudgetModal = () => {
     }
   };
   return (
-    <Modal
-      open={isOpen}
-      onOpenChange={(open) => {
-        setIsOpen(open);
-        if (!open) {
-          resetTripData();
-          setStep(1);
-        }
-      }}
-    >
-      <Modal.Trigger>
-        <Button className="btn-blue flex items-center justify-center gap-1">
+    <StepModal>
+      <StepModal.Trigger>
+        <Button className="btn-blue-border flex items-center justify-center gap-1">
           <AddSharpIcon sx={{ fontSize: 18 }} />
           예산 만들기
         </Button>
-      </Modal.Trigger>
-      <Modal.Content className="w-full top-[52px] bottom-0 translate-y-0 !p-0 rounded-b-none">
-        <Modal.Title className="p-4">
-          {step > 1 && (
-            <Button
-              className="absolute top-1/2 left-4 !p-0 -translate-y-1/2"
-              onClick={() => setStep((p) => p - 1)}
-            >
-              <ArrowBackIosNewSharpIcon sx={{ fontSize: 18 }} />
-            </Button>
-          )}
-          예산 만들기
-          <Modal.Close>
-            <Button className="absolute top-1/2 right-4 !p-0 -translate-y-1/2">
-              <CloseSharpIcon />
-            </Button>
-          </Modal.Close>
-        </Modal.Title>
-
-        <div className="flex flex-col gap-4 h-[calc(100%-60px)]">
-          {
+      </StepModal.Trigger>
+      <StepModal.Content onCloseAutoFocus={resetTripData}>
+        <StepModal.Title>예산 만들기</StepModal.Title>
+        <StepModal.StepSection
+          stepContentList={[
             {
-              1: <TitleInputContent />,
-              2: <SelectCurrencyContent onCreateBudget={onCreateBudget} />,
-            }[step]
-          }
-        </div>
-      </Modal.Content>
-    </Modal>
+              content: <TitleInputContent />,
+              nextButton: (
+                <StepModal.StepNext
+                  requiredCondition={{
+                    condition: budgetData.title.trim().length === 0,
+                    description: "이름을 입력해주세요!",
+                  }}
+                >
+                  다음
+                </StepModal.StepNext>
+              ),
+            },
+            {
+              content: <SelectCurrencyContent />,
+              nextButton: (
+                <StepModal.StepNext onLastStepHandler={onCreateBudget}>
+                  예산 만들기
+                </StepModal.StepNext>
+              ),
+            },
+          ]}
+        />
+      </StepModal.Content>
+    </StepModal>
   );
 };
 
@@ -117,7 +101,6 @@ export default CreateBudgetModal;
 
 const TitleInputContent = () => {
   const [budgetData, setBudgetData] = useAtom(budgetReqAtom);
-  const setStep = useSetAtom(stepAtom);
 
   const onChangeValueHandler = (
     e: ChangeEvent | React.MouseEvent<HTMLButtonElement | MouseEvent>
@@ -126,77 +109,56 @@ const TitleInputContent = () => {
     setBudgetData((p) => ({ ...p, [name]: value }));
   };
   return (
-    <>
-      <div className="flex-1 overflow-auto p-4">
-        <div className="flex flex-col gap-8">
-          <div className="flex flex-col gap-4 ">
-            <h2 className="text-xl font-medium">
-              예산 이름을 입력해주세요{" "}
-              <span className="text-xs font-normal text-grey-400">
-                최대 10자
-              </span>
-            </h2>
-            <Input
-              placeholder="예산 이름"
-              name="title"
-              type="text"
-              value={budgetData.title}
-              onChange={onChangeValueHandler}
-              required
-              autoFocus
-              maxLength={10}
-              className="w-full text-lg border-blue-300 rounded-xl"
-            />
-          </div>
-          <div className="flex flex-col gap-4 ">
-            <h2 className="text-xl font-medium">예산 종류를 선택해주세요 </h2>
-            <div className="flex">
-              <Button
-                className={clsx(budgetData.type === "CASH" && "btn-blue")}
-                onClick={onChangeValueHandler}
-                value={"CASH"}
-                name="type"
-              >
-                현금
-              </Button>
-              <Button
-                className={clsx(budgetData.type === "CARD" && "btn-blue")}
-                onClick={onChangeValueHandler}
-                value={"CARD"}
-                name="type"
-              >
-                카드
-              </Button>
-            </div>
+    <div className="flex-1 overflow-auto p-4">
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-4 ">
+          <h2 className="text-xl font-medium">
+            예산 이름을 입력해주세요{" "}
+            <span className="text-xs font-normal text-grey-400">최대 10자</span>
+          </h2>
+          <Input
+            placeholder="예산 이름"
+            name="title"
+            type="text"
+            value={budgetData.title}
+            onChange={onChangeValueHandler}
+            required
+            autoFocus
+            maxLength={10}
+            className="w-full text-lg border-blue-300 rounded-xl"
+          />
+        </div>
+        <div className="flex flex-col gap-4 ">
+          <h2 className="text-xl font-medium">예산 종류를 선택해주세요 </h2>
+          <div className="flex">
+            <Button
+              className={clsx(budgetData.type === "CASH" && "btn-blue")}
+              onClick={onChangeValueHandler}
+              value={"CASH"}
+              name="type"
+            >
+              현금
+            </Button>
+            <Button
+              className={clsx(budgetData.type === "CARD" && "btn-blue")}
+              onClick={onChangeValueHandler}
+              value={"CARD"}
+              name="type"
+            >
+              카드
+            </Button>
           </div>
         </div>
       </div>
-      <Button
-        onClick={() => {
-          if (budgetData.title.trim().length == 0) {
-            return alert("이름을 입력해주세요!");
-          }
-          setStep((p) => p + 1);
-        }}
-        className="btn-blue m-4"
-      >
-        다음
-      </Button>
-    </>
+    </div>
   );
 };
 
-interface SelectCurrencyContentProps {
-  onCreateBudget: (e: FormEvent) => void;
-}
-const SelectCurrencyContent = ({
-  onCreateBudget,
-}: SelectCurrencyContentProps) => {
+const SelectCurrencyContent = () => {
   const { data: currenciesQuery } = useQuery(GET_CURRENCIES);
 
   const [budgetData, setBudgetData] = useAtom(budgetReqAtom);
   const { Country } = useAtomValue(tripAtom);
-  const setStep = useSetAtom(stepAtom);
 
   const defaultCurrencies = useMemo(
     () =>
@@ -206,42 +168,37 @@ const SelectCurrencyContent = ({
     [currenciesQuery]
   );
   return (
-    <>
-      <div className="flex-1 overflow-auto p-4">
-        <div className="flex flex-col gap-4">
-          <h2 className="text-xl font-medium">
-            화페 단위를 선택해주세요{" "}
-            <span className="text-xs font-normal text-grey-400">단일 선택</span>
-          </h2>
-          <div className="flex flex-col gap-2 ">
-            {defaultCurrencies
-              .concat(currenciesQuery?.currencies ?? [])
-              .map((currency: Currency, idx: number) => (
-                <div
-                  key={currency.id + idx}
-                  onClick={() => {
-                    if (budgetData.currencyId === currency.id) {
-                      setBudgetData((p) => ({ ...p, currencyId: "" }));
-                    } else {
-                      setBudgetData((p) => ({ ...p, currencyId: currency.id }));
-                    }
-                  }}
-                  className={clsx(
-                    budgetData.currencyId === currency.id &&
-                      "bg-grey-light-400 duration-300 text-blue",
-                    "p-1.5 rounded-lg flex justify-between"
-                  )}
-                >
-                  <span className="font-medium">{currency.id}</span>
-                  <span>{currency.name}</span>
-                </div>
-              ))}
-          </div>
+    <div className="flex-1 overflow-auto p-4">
+      <div className="flex flex-col gap-4">
+        <h2 className="text-xl font-medium">
+          화페 단위를 선택해주세요{" "}
+          <span className="text-xs font-normal text-grey-400">단일 선택</span>
+        </h2>
+        <div className="flex flex-col gap-2 ">
+          {defaultCurrencies
+            .concat(currenciesQuery?.currencies ?? [])
+            .map((currency: Currency, idx: number) => (
+              <div
+                key={currency.id + idx}
+                onClick={() => {
+                  if (budgetData.currencyId === currency.id) {
+                    setBudgetData((p) => ({ ...p, currencyId: "" }));
+                  } else {
+                    setBudgetData((p) => ({ ...p, currencyId: currency.id }));
+                  }
+                }}
+                className={clsx(
+                  budgetData.currencyId === currency.id &&
+                    "bg-grey-light-400 duration-300 text-blue",
+                  "p-1.5 rounded-lg flex justify-between"
+                )}
+              >
+                <span className="font-medium">{currency.id}</span>
+                <span>{currency.name}</span>
+              </div>
+            ))}
         </div>
       </div>
-      <Button onClick={onCreateBudget} className="btn-blue m-4">
-        예산 만들기
-      </Button>
-    </>
+    </div>
   );
 };
