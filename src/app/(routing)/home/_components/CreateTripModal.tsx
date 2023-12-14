cd9e982697289dba4f5f1632e251e475bb21de16
runru1030 <runru1030@gmail.com>
2023-12-14 14:08:24 +0900

    feat : 여행 생성 모달 step 처리

--- src/app/(routing)/home/_components/CreateTripModal.tsx
+++ src/app/(routing)/home/_components/CreateTripModal.tsx
@@ -8,25 +8,32 @@ import Modal from "@components/Modal";
 import useDateSelect from "@components/useDateSelect";
 import { CREATE_TRIP } from "@lib/graphql/mutations";
 import { GET_COUNTRIES } from "@lib/graphql/queries";
+import AddSharpIcon from "@mui/icons-material/AddSharp";
 import CloseSharpIcon from "@mui/icons-material/CloseSharp";
 import { Country } from "@prisma/client";
 import clsx from "clsx";
-import { useSetAtom } from "jotai";
-import { FormEvent, useState } from "react";
-import { myTripStore, myTripsAtom } from "./MyTripProvider";
-import AddSharpIcon from "@mui/icons-material/AddSharp";
+import { useAtom, useAtomValue, useSetAtom } from "jotai";
+import { atomWithReset, useResetAtom } from "jotai/utils";
 import Image from "next/image";
+import { FormEvent, useEffect, useState } from "react";
+import { myTripStore, myTripsAtom } from "./MyTripProvider";
+import ArrowBackIosNewSharpIcon from "@mui/icons-material/ArrowBackIosNewSharp";
+
+const tripReqAtom = atomWithReset({
+  title: "",
+  countryId: "",
+  startedAt: new Date(),
+  endedAt: new Date(),
+});
+
 const CreateTripModal = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [step, setStep] = useState(1);
 
-  const { data: countriesQuery } = useQuery(GET_COUNTRIES);
+  const tripData = useAtomValue(tripReqAtom);
+  const resetTripData = useResetAtom(tripReqAtom);
   const userId = "1";
-
-  const [tripData, setTripData] = useState({
-    title: "",
-    countryId: "",
-  });
-  const { selectionRange, handleSelectDate, resetSelectDate } = useDateSelect();
+  const setMyTrips = useSetAtom(myTripsAtom, { store: myTripStore });
 
   const [createTrip] = useMutation(CREATE_TRIP, {
     onCompleted: (res) => {
@@ -34,7 +41,6 @@ const CreateTripModal = () => {
       setIsOpen(false);
     },
   });
-  const setMyTrips = useSetAtom(myTripsAtom, { store: myTripStore });
 
   const onCreateTrip = (e: FormEvent) => {
     try {
@@ -47,107 +53,160 @@ const CreateTripModal = () => {
       createTrip({
         variables: {
           ...tripData,
-          startedAt: selectionRange.startDate,
-          endedAt: selectionRange.startDate,
           userId,
         },
       });
-    } catch (error) {
-      console.error(error);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error(error);
+        alert(error.message);
+      }
     }
   };
-
   return (
     <Modal
       open={isOpen}
       onOpenChange={(open) => {
         setIsOpen(open);
         if (!open) {
-          setTripData({
-            title: "",
-            countryId: "",
-          });
-          resetSelectDate();
+          resetTripData();
+          setStep(1);
         }
       }}
     >
       <Modal.Trigger>
-        <Button className="fixed bottom-10 left-1/2 -translate-x-1/2 rounded-full px-6 shadow-md btn-blue text-lg flex items-center gap-2 pl-2">
-          <AddSharpIcon sx={{ fontSize: 24 }} />
+        <Button className="btn-blue flex items-center justify-center gap-1">
+          <AddSharpIcon sx={{ fontSize: 18 }} />
           여행 만들기
         </Button>
       </Modal.Trigger>
       <Modal.Content className="w-full top-[52px] bottom-0 translate-y-0 !p-0 rounded-b-none">
         <Modal.Title className="p-4">
-          나의 여행 만들기
+          {step > 1 && (
+            <Button
+              className="absolute top-1/2 left-4 !p-0 -translate-y-1/2"
+              onClick={() => setStep((p) => p - 1)}
+            >
+              <ArrowBackIosNewSharpIcon sx={{ fontSize: 18 }} />
+            </Button>
+          )}
+          여행 만들기
           <Modal.Close>
-            <Button className="absolute top-1/2 left-4 !p-0 -translate-y-1/2">
+            <Button className="absolute top-1/2 right-4 !p-0 -translate-y-1/2">
               <CloseSharpIcon />
             </Button>
           </Modal.Close>
         </Modal.Title>
 
-        <form
-          onSubmit={onCreateTrip}
-          className="flex flex-col gap-4 overflow-y-auto px-4 py-4 h-[calc(100%-60px)]"
-        >
-          <Input
-            placeholder="여행 이름을 입력해주세요"
-            type="text"
-            value={tripData.title}
-            onChange={(e) =>
-              setTripData((p) => ({ ...p, title: e.target.value }))
+        <div className="flex flex-col gap-4 h-[calc(100%-60px)]">
+          <div className="flex-1 overflow-auto p-4">
+            {
+              {
+                1: <TitleInputContent />,
+                2: <SelectCountryContent />,
+                3: <SelectDateContent />,
+              }[step]
             }
-            required
-            className="w-full text-lg font-medium border border-blue-300 rounded-lg"
-          />
-
-          <div className="flex flex-col gap-2">
-            <h2 className=" font-medium text-lg">여행 국가</h2>
-            <div className="h-[30vh] overflow-scroll flex flex-col gap-2 ">
-              {countriesQuery?.countries?.map((country: Country) => (
-                <div
-                  key={country.id}
-                  onClick={() => {
-                    if (tripData.countryId === country.id) {
-                      setTripData((p) => ({ ...p, countryId: "" }));
-                    } else {
-                      setTripData((p) => ({ ...p, countryId: country.id }));
-                    }
-                  }}
-                  className={clsx(
-                    tripData.countryId === country.id &&
-                      "bg-grey-light-400 duration-300 text-blue",
-                    "p-1.5 rounded-md flex justify-between"
-                  )}
-                >
-                  {country.name}
-                  <Image
-                    src={country.flag_img ?? ""}
-                    width={40}
-                    height={100}
-                    alt="국기"
-                  />
-                </div>
-              ))}
-            </div>
           </div>
-
-          <div className="flex flex-col gap-2">
-            <h2 className=" font-medium text-lg">여행 날짜</h2>
-            <DateSelector
-              ranges={[selectionRange]}
-              onChange={handleSelectDate}
-            />
-          </div>
-
-          <Button type="submit" className="btn-blue">
-            여행 만들기
-          </Button>
-        </form>
+          {step !== 3 ? (
+            <Button
+              onClick={() => setStep((p) => p + 1)}
+              className="btn-blue m-4"
+            >
+              다음
+            </Button>
+          ) : (
+            <Button onClick={onCreateTrip} className="btn-blue m-4">
+              여행 만들기
+            </Button>
+          )}
+        </div>
       </Modal.Content>
     </Modal>
   );
 };
 
 export default CreateTripModal;
+
+const TitleInputContent = () => {
+  const [tripData, setTripData] = useAtom(tripReqAtom);
+  return (
+    <div className="flex flex-col gap-4 ">
+      <h2 className="text-xl font-medium">
+        여행 이름을 입력해주세요{" "}
+        <span className="text-xs font-normal text-grey-400">최대 10자</span>
+      </h2>
+      <Input
+        placeholder="여행 이름"
+        type="text"
+        value={tripData.title}
+        onChange={(e) => setTripData((p) => ({ ...p, title: e.target.value }))}
+        required
+        autoFocus
+        maxLength={10}
+        className="w-full text-lg border-blue-300 rounded-xl"
+      />
+    </div>
+  );
+};
+const SelectCountryContent = () => {
+  const { data: countriesQuery } = useQuery(GET_COUNTRIES);
+  const [tripData, setTripData] = useAtom(tripReqAtom);
+  return (
+    <div className="flex flex-col gap-4">
+      <h2 className="text-xl font-medium">
+        여행 국가를 선택해주세요{" "}
+        <span className="text-xs font-normal text-grey-400">단일 선택</span>
+      </h2>
+      <div className="flex flex-col gap-2 ">
+        {countriesQuery?.countries?.map((country: Country) => (
+          <div
+            key={country.id}
+            onClick={() => {
+              if (tripData.countryId === country.id) {
+                setTripData((p) => ({ ...p, countryId: "" }));
+              } else {
+                setTripData((p) => ({ ...p, countryId: country.id }));
+              }
+            }}
+            className={clsx(
+              tripData.countryId === country.id &&
+                "bg-grey-light-400 duration-300 text-blue",
+              "p-1.5 rounded-lg flex justify-between"
+            )}
+          >
+            {country.name}
+            <Image
+              src={country.flag_img ?? ""}
+              width={40}
+              height={100}
+              alt="국기"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+const SelectDateContent = () => {
+  const [tripData, setTripData] = useAtom(tripReqAtom);
+  const { selectionRange, handleSelectDate } = useDateSelect({
+    defaultStartDate: tripData.startedAt,
+    defaultEndDate: tripData.endedAt,
+  });
+
+  useEffect(() => {
+    setTripData((p) => ({
+      ...p,
+      startedAt: selectionRange.startDate,
+      endedAt: selectionRange.endDate,
+    }));
+  }, [selectionRange]);
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h2 className="text-xl font-medium">여행 날짜를 선택해주세요 </h2>
+      <DateSelector ranges={[selectionRange]} onChange={handleSelectDate} />
+    </div>
+  );
+};
