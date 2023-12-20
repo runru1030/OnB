8d8ea7229d5197f6d8aaa609f47025ff43bfe043
runru1030 <runru1030@gmail.com>
2023-12-20 19:02:11 +0900

    feat : next-auth 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
+import { ApolloProviders } from "@app/_components/ApolloProvider";
 import "@styles/globals.css";
 import localFont from "next/font/local";
 import GlobalProvider from "./_components/GlobalProvider";
-import { ApolloProviders } from "@app/_components/ApolloProvider";
-import LOGO from "public/assets/logo_sm.svg";
 
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
-
+import AuthProvider from "./_components/AuthProvider";
 const pretendard = localFont({
   src: "../../public/font/PretendardVariable.woff2",
   display: "swap",
@@ -21,7 +20,9 @@ export default function RootLayout({
     <html lang="en" className={pretendard.className}>
       <body>
         <ApolloProviders>
-          <GlobalProvider>{children}</GlobalProvider>
+          <AuthProvider>
+            <GlobalProvider>{children}</GlobalProvider>
+          </AuthProvider>
         </ApolloProviders>
       </body>
     </html>
