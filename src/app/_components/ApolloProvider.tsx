a30d16c1c1642d01dcc02ff9f3b2dd1bda74bba7
runru1030 <runru1030@gmail.com>
2023-12-21 13:56:54 +0900

    chore : domain 변수 처리

--- src/app/_components/ApolloProvider.tsx
+++ src/app/_components/ApolloProvider.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren } from "react";
 
 export const ApolloProviders = ({ children }: PropsWithChildren) => {
   const client = new ApolloClient({
-    uri: `${process.env.NEXT_SERVER_DOMAIN}/api/graphql`,
+    uri: `${process.env.NEXT_PUBLIC_DOMAIN}/api/graphql`,
     cache: new InMemoryCache(),
     ssrMode: true,
   });
