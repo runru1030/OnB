41efa2d09b816563b2855189b9eae6b27d37a2ce
runru1030 <runru1030@gmail.com>
2023-12-22 16:36:29 +0900

    refactor : 로딩 UI 컴포넌트

---
+++ src/components/LoadingDots.tsx
@@ -0,0 +1,9 @@
+export default function LoadingDots() {
+  return (
+    <div className="flex justify-center gap-3">
+      <div className="h-3 w-3 animate-[bounce_1s_infinite_100ms] rounded-full bg-grey-400" />
+      <div className="h-3 w-3 animate-[bounce_1s_infinite_200ms] rounded-full bg-grey-400" />
+      <div className="h-3 w-3 animate-[bounce_1s_infinite_300ms] rounded-full bg-grey-400" />
+    </div>
+  );
+}
