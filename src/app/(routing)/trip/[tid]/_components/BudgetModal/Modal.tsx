535d2979d4a2543813587fa913fd816e1875dce5
runru1030 <70322673+runru1030@users.noreply.github.com>
2023-12-28 17:56:03 +0900

    Feature/budget 모달 내 채우기 지출 36 (#38)
    * chore : budget modal 배치 수정
    
    * design : 스타일 수정

--- src/app/(routing)/trip/[tid]/_components/BudgetModal/Modal.tsx
+++ src/app/(routing)/trip/[tid]/_components/BudgetModal/Modal.tsx
@@ -10,10 +10,11 @@ import PaymentsTwoToneIcon from "@mui/icons-material/PaymentsTwoTone";
 import clsx from "clsx";
 import { atom, useAtom, useSetAtom } from "jotai";
 import dynamic from "next/dynamic";
-import { PropsWithChildren, useMemo } from "react";
+import { PropsWithChildren, useMemo, useState } from "react";
 import { BudgetQueryData } from "../../_types";
 import EditBudgetModalWithTrigger from "../EditBudgetModalWithTrigger";
 import CreateIncomeModal from "../CreateIncomeModal";
+import KeyboardArrowDownSharpIcon from "@mui/icons-material/KeyboardArrowDownSharp";
 import CreateExpenseModal from "../CreateExpenseModal";
 
 const IncomeExpenseList = dynamic(() => import("../IncomeExpenseList"));
@@ -29,6 +30,7 @@ const InternalBudgetModal = () => {
   const [budgetData, setBudgetData] = useAtom(budgetAtom);
   const { totalIncomes, totalIncomesKRW, totalExpenses, totalExpensesKRW } =
     useMemo(() => getSumOfBudget(budgetData as BudgetQueryData), [budgetData]);
+  const [openMore, setOpenMore] = useState(false);
   useQuery(budget.GET_BUDGET, {
     variables: { id: budgetData?.id },
     onCompleted: (data) => {
@@ -38,7 +40,10 @@ const InternalBudgetModal = () => {
 
   return (
     <Modal open={openAtom} onOpenChange={setOpenAtom}>
-      <Modal.Content className="w-full top-[52px] bottom-0 translate-y-0 !p-0 rounded-b-none">
+      <Modal.Content
+        className="w-full top-[52px] bottom-0 translate-y-0 !p-0 rounded-b-none"
+        onCloseAutoFocus={() => setOpenMore(false)}
+      >
         <Modal.Title className="p-4">
           <EditBudgetModalWithTrigger budget={budgetData as BudgetQueryData} />
           {budgetData?.title}
@@ -49,7 +54,7 @@ const InternalBudgetModal = () => {
           </Modal.Close>
         </Modal.Title>{" "}
         <div className="flex flex-col h-[calc(100%-60px)] overflow-auto">
-          <div className="flex-1 p-4 flex flex-col gap-4">
+          <div className="p-4 flex flex-col gap-4">
             <div className="flex flex-col pb-2">
               <div className="text-3xl flex gap-1 justify-end items-end">
                 <span className=" font-medium">
@@ -73,48 +78,85 @@ const InternalBudgetModal = () => {
                 <span className="text-base">{budgetData?.Currency.id}</span>
               </span>
               <span className="text-base text-red-300">
-                = {totalExpensesKRW?.toLocaleString()} 원
-              </span>
-            </div>
-            <div className="flex flex-col items-end text-sm text-grey-300">
-              <span>총 예산</span>
-              <span>= {totalIncomesKRW?.toLocaleString()} 원</span>
-            </div>
-            <div className="flex flex-col items-end text-sm text-grey-300">
-              <span>잔여 예산</span>
-              <span>
-                = {(totalIncomesKRW - totalExpensesKRW)?.toLocaleString()} 원
+                = {totalExpensesKRW?.toLocaleString()}원
               </span>
             </div>
 
-            <div className="flex justify-between items-center text-grey-300">
-              <h2 className="">예산 종류</h2>
-              <div className="flex gap-4">
-                <div className={clsx("py-2 text-sm flex gap-2 items-center")}>
-                  {
-                    {
-                      CASH: (
-                        <>
-                          <PaymentsTwoToneIcon />
-                          현금
-                        </>
-                      ),
-                      CARD: (
-                        <>
-                          <PaymentTwoToneIcon />
-                          카드
-                        </>
-                      ),
-                    }[budgetData?.type as "CARD" | "CASH"]
-                  }
-                </div>
-              </div>
+            <div className="flex w-full gap-4">
+              <CreateIncomeModal.Trigger
+                budget={budgetData as BudgetQueryData}
+                className="btn-grey-border bg-white w-full shadow-normal"
+              >
+                채우기
+              </CreateIncomeModal.Trigger>
+              <CreateExpenseModal.Trigger
+                budget={budgetData as BudgetQueryData}
+                className="btn-red w-full shadow-normal"
+              >
+                지출하기
+              </CreateExpenseModal.Trigger>
             </div>
-            <div className="flex justify-between items-center text-grey-300">
-              <h2 className="">통화</h2>
-              <div className="flex gap-4">
-                <span className="font-medium">{budgetData?.Currency.id}</span>
-                <span>{budgetData?.Currency.name}</span>
+            <div className="flex flex-col">
+              <div
+                className="flex items-center text-grey-300"
+                onClick={() => setOpenMore((p) => !p)}
+              >
+                예산 정보
+                <KeyboardArrowDownSharpIcon
+                  className={clsx(
+                    !openMore ? "rotate-0" : "rotate-180",
+                    "duration-300 text-grey-300"
+                  )}
+                />
+              </div>
+              <div
+                className={clsx(
+                  openMore ? "h-[144px] pt-2" : "h-[0px]",
+                  "duration-300 overflow-hidden flex flex-col gap-2 text-grey-200"
+                )}
+              >
+                <div className="flex justify-between items-center">
+                  <h2 className="">총 예산 (KRW)</h2>
+                  <span>{totalIncomesKRW?.toLocaleString()}원</span>
+                </div>
+                <div className="flex justify-between items-center">
+                  <h2 className="">잔여 예산 (KRW)</h2>
+                  <span>
+                    {(totalIncomesKRW - totalExpensesKRW)?.toLocaleString()}원
+                  </span>
+                </div>
+                <div className="flex justify-between items-center">
+                  <h2 className="">예산 종류</h2>
+                  <div className="flex gap-4">
+                    <div className={clsx("text-sm flex gap-2 items-center")}>
+                      {
+                        {
+                          CASH: (
+                            <>
+                              <PaymentsTwoToneIcon />
+                              현금
+                            </>
+                          ),
+                          CARD: (
+                            <>
+                              <PaymentTwoToneIcon />
+                              카드
+                            </>
+                          ),
+                        }[budgetData?.type as "CARD" | "CASH"]
+                      }
+                    </div>
+                  </div>
+                </div>
+                <div className="flex justify-between items-center">
+                  <h2 className="">통화</h2>
+                  <div className="flex gap-4">
+                    <span className="font-medium">
+                      {budgetData?.Currency.id}
+                    </span>
+                    <span>{budgetData?.Currency.name}</span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -124,20 +166,6 @@ const InternalBudgetModal = () => {
                 dataList={[...budgetData.incomes, ...budgetData.expenses]}
               />
             )}
-          </div>
-          <div className="flex w-full gap-4 p-4 mb-2 sticky bottom-4">
-            <CreateIncomeModal.Trigger
-              budget={budgetData as BudgetQueryData}
-              className="btn-grey-border bg-white w-full shadow-normal"
-            >
-              채우기
-            </CreateIncomeModal.Trigger>
-            <CreateExpenseModal.Trigger
-              budget={budgetData as BudgetQueryData}
-              className="btn-red w-full shadow-normal"
-            >
-              지출하기
-            </CreateExpenseModal.Trigger>
           </div>
         </div>
       </Modal.Content>
