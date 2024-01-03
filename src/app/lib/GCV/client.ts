50c6b991b5ba42e225d5373de30d0dd5cbaf10b8
runru1030 <runru1030@gmail.com>
2024-01-04 05:03:23 +0900

    fix: vercel path

--- src/app/lib/GCV/client.ts
+++ src/app/lib/GCV/client.ts
@@ -2,5 +2,5 @@ import { ImageAnnotatorClient } from "@google-cloud/vision";
 import path from "path";
 
 export default new ImageAnnotatorClient({
-  keyFilename: path.resolve("./src/app/lib/GCV/service-account-key.json"),
+  keyFilename: path.resolve("public/service-account-key.json"),
 });
