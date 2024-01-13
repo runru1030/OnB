a8feafbc79f2b228a7a57c459a644bc2a5a2f817
runru1030 <runru1030@gmail.com>
2024-01-13 17:39:49 +0900

    fix : 구글 api 인증

--- src/app/lib/GCV/client.ts
+++ src/app/lib/GCV/client.ts
@@ -1,6 +1,13 @@
 import { ImageAnnotatorClient } from "@google-cloud/vision";
-import path from "path";
 
 export default new ImageAnnotatorClient({
-  keyFilename: path.join(process.cwd(), "public", "service-account-key.json"),
+  credentials: {
+    type: "service_account",
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
+    token_url: process.env.GOOGLE_TOKEN_URL,
+    universe_domain: "googleapis.com",
+  },
 });
